c4eeb9020e409c15f31deeb2ee03595bc38ffde3
Jeff Smith <jeff@thoughtbot.com>
2014-09-30 19:52:09 -0700

    finish required assignments

--- MailboxViewController.swift
+++ MailboxViewController.swift
@@ -10,24 +10,53 @@ import UIKit
 
 class MailboxViewController: UIViewController {
 
+    @IBOutlet weak var feedImageView: UIImageView!
+    @IBOutlet weak var Feed: UIImageView!
     @IBOutlet weak var inboxScrollView: UIScrollView!
     @IBOutlet weak var messageView: UIView!
     @IBOutlet weak var messageImageView: UIImageView!
     @IBOutlet weak var leftMessageIconImageView: UIImageView!
     @IBOutlet weak var rightMessageIconImageView: UIImageView!
-    
-    // Define color variables
+    @IBOutlet weak var listImageView: UIImageView!
+    @IBOutlet weak var rescheduleImageView: UIImageView!
+    @IBOutlet var listTapGestureRecognizer: UITapGestureRecognizer!
+    @IBOutlet var rescheduleTapGestureRecognizer: UITapGestureRecognizer!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        inboxScrollView.contentSize = CGSize(width: 320, height: (1202 + 86 + 37 + 42))
 
+        inboxScrollView.contentSize = CGSize(width: 320, height: (1202 + 86 + 37 + 42))
         var messagePanRecognizer = UIPanGestureRecognizer(target: self, action: "onMessagePan:")
+        
         messageView.addGestureRecognizer(messagePanRecognizer)
+        listTapGestureRecognizer = UITapGestureRecognizer(target: self, action: "onListTap")
+        listImageView.addGestureRecognizer(listTapGestureRecognizer)
+        rescheduleTapGestureRecognizer = UITapGestureRecognizer(target: self, action: "onRescheduleTap")
+        rescheduleImageView.addGestureRecognizer(rescheduleTapGestureRecognizer)
+    }
+    
+    func sendMessage() {
+        self.messageView.removeFromSuperview()
+        UIView.animateWithDuration(0.25, animations: { self.feedImageView.frame.origin.y = self.feedImageView.frame.origin.y - 86
+        }, completion: nil)
         
     }
+    
+    func onListTap() {
+        UIView.animateWithDuration(0.25, animations: {
+            self.listImageView.alpha = 0
+            }, completion: nil
+        )
+    }
 
+    func onRescheduleTap() {
+        UIView.animateWithDuration(0.25, animations: {
+            self.rescheduleImageView.alpha = 0
+            }, completion: nil
+        )
+    }
+    
+    
     @IBAction func onMessagePan(messagePanRecognizer: UIPanGestureRecognizer) {
         
         var point = messagePanRecognizer.locationInView(view)
@@ -37,35 +66,42 @@ class MailboxViewController: UIViewController {
             
             var color: UIColor = UIColor(red: 0.89, green: 0.89, blue: 0.89, alpha: 1)
 
-            leftMessageIconImageView.alpha = (transform.x/75) - 0.1
-            rightMessageIconImageView.alpha = (-transform.x/75) - 0.1
+            leftMessageIconImageView.alpha = (transform.x / 75) - 0.1
+            rightMessageIconImageView.alpha = (-transform.x / 75) - 0.1
             
             self.messageImageView.center.x = messageView.center.x + transform.x
 
             if self.messageImageView.frame.origin.x > 60 {
                 println("Archive")
-//                leftMessageIconImageView.alpha = 0
                 color = UIColor(red: 0.38, green: 0.85, blue: 0.38, alpha: 1)
+                leftMessageIconImageView.image = UIImage(named: "archive_icon")
             }
 
-            if self.messageImageView.frame.origin.x > 260 {
+            if self.messageImageView.frame.origin.x > 200 {
                 println("Delete")
                 color = UIColor(red: 0.93, green: 0.33, blue: 0.05, alpha: 1)
                 leftMessageIconImageView.image = UIImage(named: "delete_icon")
             }
             
-            
             if self.messageImageView.frame.origin.x < -60 {
                 println("Later")
-//                rightMessageIconImageView.alpha = 0
                 color = UIColor(red: 1, green: 0.8, blue: 0, alpha: 1)
+                leftMessageIconImageView.image = UIImage(named: "later_icon")
             }
             
-
-            if self.messageImageView.frame.origin.x < -260 {
+            if self.messageImageView.frame.origin.x < -200 {
                 println("Categorize")
                 color = UIColor(red: 0.84, green: 0.65, blue: 0.45, alpha: 1)
                 rightMessageIconImageView.image = UIImage(named: "list_icon")
+            }
+            
+            
+            if transform.x > 60 {
+                leftMessageIconImageView.frame.origin.x = 15 + transform.x - 60
+            }
+            
+            if transform.x < -60 {
+                rightMessageIconImageView.frame.origin.x = 275 + transform.x + 60
             }
             
             self.messageView.backgroundColor = color
@@ -77,35 +113,53 @@ class MailboxViewController: UIViewController {
                     0.4,
                     animations: {
                         self.messageImageView.frame.origin.x += 320
-                }, completion: nil)
-            }
-            
-            if self.messageImageView.frame.origin.x > 260 {
+                }, completion: { (finished: Bool) in
+                    self.sendMessage() })
+            } else if self.messageImageView.frame.origin.x > 260 {
                 println("Delete Ended")
                 UIView.animateWithDuration(
                     0.4,
                     animations: {
-                    self.messageImageView.frame.origin.x += 320
+                        self.messageImageView.frame.origin.x += 320
                 }, completion: nil)
-            }
-            
-            if self.messageImageView.frame.origin.x < -60 {
+            } else if self.messageImageView.frame.origin.x < -60 &&
+                      self.messageImageView.frame.origin.x > -260 {
                 println("Later Ended")
                 UIView.animateWithDuration(
                     0.4,
                     animations: {
                         self.messageImageView.frame.origin.x -= 320
-                }, completion: nil)
-            }
-            
-            
-            if self.messageImageView.frame.origin.x < -260 {
+                    }, completion: { (finished: Bool) in
+                        self.view.bringSubviewToFront(self.rescheduleImageView)
+                        UIView.animateWithDuration(0.25, animations: {
+                            self.rescheduleImageView.alpha = 1.0
+                            }, completion: nil)
+                })
+            } else if self.messageImageView.frame.origin.x < -260 {
                 println("Categorize Ended")
                 UIView.animateWithDuration(
                     0.4,
                     animations: {
                         self.messageImageView.frame.origin.x -= 320
-                }, completion: nil)
+                    }, completion: { (finished: Bool) in
+                        self.view.bringSubviewToFront(self.listImageView)
+                        UIView.animateWithDuration(0.25, animations: {
+                            self.rescheduleImageView.alpha = 1.0
+                        }, completion: nil)
+                })
+            
+            } else {
+                println("Reset Message")
+                UIView.animateWithDuration(
+                    0.4,
+                    delay: 0,
+                    usingSpringWithDamping: 0.8,
+                    initialSpringVelocity: 3,
+                    options: nil,
+                    animations: {
+                        self.messageImageView.center.x = self.messageView.center.x
+                    },
+                    completion: nil)
             }
         }
     }
